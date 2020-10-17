3bb407ebf32021b12240a0280c1d5282fa5d4622
Gogs <richardpillar@googlemail.com>
2020-10-17 19:52:28 +0100

    Finally add the db tables sql.

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE contacts (firstname text, lastname text);
+CREATE TABLE users (userName text, password text);
+CREATE TABLE channel (channelName text, status text, userName text, update_date text);
+CREATE TABLE category(channel text, categoryKey text, categoryName text);
+CREATE TABLE credentials (channel text, credentials text, shopId integer, shopName text, active integer);
+CREATE TABLE transactionData(channel TEXT, shopName TEXT, retrieval_date TEXT, start_date TEXT,end_date TEXT,schedule_date TEXT, schedule_time TEXT,processed INTEGER,transaction_count INTEGER);
+CREATE TABLE `transaction`(id INTEGER, productId INTEGER, category TEXT, date TEXT, time TEXT, quantity INTEGER, customer TEXT, channelId INTEGER, transactionType TEXT, amount INTEGER );
+CREATE TABLE shops(channel TEXT, shopId INTEGER PRIMARY KEY AUTOINCREMENT, shopName TEXT, shopDescription TEXT, status TEXT, update_date TEXT , userName text);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE categorydata(channel text, customCategoryName text, categoryName text);
+CREATE TABLE transactionScheduleData(channel text, shopName text, scheduleTime text);

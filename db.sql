9c0eb6df4c2998679d214c75f4c392f656ea7803
rpillar <richardpillar@googlemail.com>
2021-05-04 19:34:40 +0100

    Updates - from MACOS to Windows 10

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (userName text, password text);
 CREATE TABLE channel (channelName text, status text, userName text, update_date text);
 CREATE TABLE category(channel text, categoryKey text, categoryName text);
 CREATE TABLE credentials (channel text, credentials text, shopId integer, shopName text, active integer);
-CREATE TABLE transactionData(channel TEXT, shopName TEXT, retrieval_date TEXT, start_date TEXT,end_date TEXT,schedule_date TEXT, schedule_time TEXT,processed INTEGER,transaction_count INTEGER);
+CREATE TABLE transactionData(channel TEXT, shopName TEXT, retrievalDate TEXT, start_date TEXT,end_date TEXT,scheduleDate TEXT, scheduleTime TEXT,processed INTEGER,transaction_count INTEGER);
 CREATE TABLE transactions(id INTEGER, productId INTEGER, category TEXT, date TEXT, time TEXT, quantity INTEGER, customer TEXT, channelId INTEGER, transactionType TEXT, amount INTEGER );
 CREATE TABLE shops(channel TEXT, shopId INTEGER PRIMARY KEY AUTOINCREMENT, shopName TEXT, shopDescription TEXT, status TEXT, update_date TEXT , userName text);
 CREATE TABLE sqlite_sequence(name,seq);
